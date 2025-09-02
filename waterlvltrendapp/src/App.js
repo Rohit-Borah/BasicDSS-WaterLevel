72b073c6e2f189f8597fa9d6a3afca610ada9b1a
Rohit-Borah <hie.rohitb@gmail.com>
2025-09-02 10:40:06 +0530

    dashboard title

--- waterlvltrendapp/src/App.js
+++ waterlvltrendapp/src/App.js
@@ -1,7 +1,12 @@
 import WaterLevelMap from "./components/WaterLevelMap";
 
 function App() {
-  return <WaterLevelMap />;
+  return (
+    <div style={{ padding: "20px"}}>
+      <h1 style={{ fontSize: "25px" }}>TEST : Water level Monitoring Dashboard</h1>
+      <WaterLevelMap />
+      </div>
+  );
 }
 
 export default App;
